0f86341076600a2420b391677597b69f1abb860d
Adrien <adrien.laaboudi@gmail.com>
2023-03-28 18:02:37 +0200

    frontend build time test with new process

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Fix the pipeline
         </a>
       </header>
     </div>
