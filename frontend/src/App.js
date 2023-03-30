c0dcbbfc4d51c3adbd3a6ce8de6d02cc260bed66
Adrien <adrien.laaboudi@gmail.com>
2023-03-30 20:52:11 +0200

    restoring the removal of docker volume build. Adding a change for testing.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Fix the pipeline testprod
+          Fix the pipeline testprod2
         </a>
       </header>
     </div>
