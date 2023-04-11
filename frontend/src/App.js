6e2894c5f04faa218eabffd7aab1b6b4110a488b
Adrien <adrien.laaboudi@gmail.com>
2023-04-11 17:10:01 +0200

    Investigation about cache space ongoing.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Fix the pipeline testcache
+          Fix the pipeline testcache2
         </a>
       </header>
     </div>
