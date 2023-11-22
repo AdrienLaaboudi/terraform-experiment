bf268d8a02d6c10b52597808d21b3fbb959f6f3b
Adrien Laâboudi <adrien.laaboudi@gmail.com>
2023-11-22 16:53:10 +0100

    Pipeline triggering test

--- backend/test9/apps.py
+++ backend/test9/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class Test9Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'test9'
+
+# Something shady is appearing there, why the prod pipeline triggers on dev?
